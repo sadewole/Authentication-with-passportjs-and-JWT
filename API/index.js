cd9ebb6fd2d3f7cf2b7db6b1a59033ba6d3e86ca
Samuel Adewole <samueladewole15@gmail.com>
2019-02-27 03:20:36 +0100

    initialize the server and database setup

---
+++ API/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+	'mongodb://localhost:27017/loginAuth',
+	{
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	},
+	() => {
+		console.log('DB connected');
+	}
+);
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// error handler
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error);
+});
+app.use((err, req, res, next) => {
+	const error = app.get('env') === 'development' ? err : {};
+
+	res.status(error.status || 500).json({
+		error: {
+			message: error.message
+		}
+	});
+});
+const port = process.env.port || 3000;
+app.listen(port, () => {
+	console.log(`Now listening to port: ${port}`);
+});
